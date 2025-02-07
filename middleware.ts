13666ab5d8e107dcc2182e943520cb8132572d08
Vikas <101702902+NotYourEren@users.noreply.github.com>
2025-02-07 21:05:14 +0530

    some changes

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+
+const isPublicRoute = createRouteMatcher([
+    '/sign-in',
+    '/sign-up',
+    '/',
+    'home',
+])
+
+const isPublicApi = createRouteMatcher([
+    '/api/videos',
+])
+
+export default clerkMiddleware( async (auth, req) => {
+    const {userId} = await auth()
+    const currentUrl = new URL(req.url)
+    const isHomePage = currentUrl.pathname === '/home'
+    const isApiRequest = currentUrl.pathname.startsWith('/api')
+
+    if( userId && isPublicRoute(req) && !isHomePage ){
+        return NextResponse.redirect(new URL('/home', req.url))
+    }
+
+    //not logged in
+    if(!userId){
+        //if user is not logged in and trying to access a private route
+        if(!isPublicRoute && !isApiRequest){
+            return NextResponse.redirect(new URL('/sign-in', req.url))
+        }
+        //if the req is for a protect api route  and user is not logged in
+        if(isApiRequest && !isPublicApi(req)){
+            return NextResponse.redirect(new URL('/sign-in', req.url))
+        }
+    }
+    return NextResponse.next()
+})
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}
---
+++ src/app/(app)/social-share/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SocialShare() {
+  return (
+    <div>socialShare</div>
+  )
+}
+
+export default SocialShare
